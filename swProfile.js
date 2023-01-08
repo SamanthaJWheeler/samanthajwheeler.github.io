ba4f28f537a2560f3520fd8b76367b5146ddedb5
SamanthaJWheeler <115271437+SamanthaJWheeler@users.noreply.github.com>
2023-01-08 15:47:16 -0500

    Add animation to Intro Text

--- swProfile.js
+++ swProfile.js
@@ -1,8 +1,30 @@
 // Animiations
 
-const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml1 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-// tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
-tl.to(".text", { y: "-100%", duration: 1.5, delay: 0});
-tl.to(".slider", { y: "-100%", duration: 1.5 }, "-=1");
-
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml1 .letter',
+    scale: [0.3,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 600,
+    delay: (el, i) => 70 * (i+1)
+  }).add({
+    targets: '.ml1 .line',
+    scaleX: [0,1],
+    opacity: [0.5,1],
+    easing: "easeOutExpo",
+    duration: 700,
+    offset: '-=875',
+    delay: (el, i, l) => 80 * (l - i)
+  }).add({
+    targets: '.ml1',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
